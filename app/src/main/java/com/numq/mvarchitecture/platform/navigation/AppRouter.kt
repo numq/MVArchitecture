f183c50380159a59799802e5d2d3c71e037da873
numq <numq.dev@gmail.com>
2022-07-31 13:26:07 +0300

    Provide permission changes

--- app/src/main/java/com/numq/mvarchitecture/platform/navigation/AppRouter.kt
+++ app/src/main/java/com/numq/mvarchitecture/platform/navigation/AppRouter.kt
@@ -1,30 +1,23 @@
 package com.numq.mvarchitecture.platform.navigation
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Close
-import androidx.compose.material.icons.rounded.Done
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.PermissionState
-import com.google.accompanist.permissions.isGranted
-import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import com.numq.mvarchitecture.platform.permission.PermissionsRequired
 import com.numq.mvarchitecture.presentation.feature.mvc.MvcScreen
 import com.numq.mvarchitecture.presentation.feature.mvi.MviScreen
 import com.numq.mvarchitecture.presentation.feature.mvp.MvpScreen
 import com.numq.mvarchitecture.presentation.feature.mvvm.MvvmScreen
-import kotlinx.coroutines.runBlocking
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
@@ -34,14 +27,12 @@ fun AppRouter() {
     val scaffoldState = rememberScaffoldState()
     val navBackStackEntry by navController.currentBackStackEntryAsState()
 
-    val permissions = rememberMultiplePermissionsState(
-        listOf(
-            android.Manifest.permission.ACCESS_NETWORK_STATE,
-            android.Manifest.permission.INTERNET
-        )
+    val permissions = listOf(
+        android.Manifest.permission.ACCESS_NETWORK_STATE,
+        android.Manifest.permission.INTERNET
     )
 
-    if (permissions.allPermissionsGranted) {
+    PermissionsRequired(permissions) {
         Scaffold(scaffoldState = scaffoldState, bottomBar = {
             BottomNavigation(navController, navBackStackEntry)
         }) {
@@ -67,59 +58,6 @@ fun AppRouter() {
                         MviScreen(Route.Mvi, scaffoldState)
                     }
                 }
-            }
-        }
-    } else {
-        NoPermissions(permissions.permissions) {
-            runBlocking {
-                permissions.launchMultiplePermissionRequest()
-            }
-        }
-    }
-}
-
-@OptIn(ExperimentalPermissionsApi::class)
-@Composable
-fun NoPermissions(permissions: List<PermissionState>, grant: () -> Unit) {
-    Box(
-        Modifier
-            .fillMaxSize()
-            .padding(8.dp), contentAlignment = Alignment.Center
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Text(
-                "You must grant required permissions",
-                textAlign = TextAlign.Center
-            )
-            Spacer(Modifier.height(16.dp))
-            Divider()
-            Spacer(Modifier.height(32.dp))
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                permissions.forEach {
-                    Row(
-                        Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.Center,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Icon(
-                            if (it.status.isGranted) Icons.Rounded.Done else Icons.Rounded.Close,
-                            "",
-                            tint = if (it.status.isGranted) Color.Green else Color.Red
-                        )
-                        Text(
-                            it.permission.split(".").last(),
-                            color = if (it.status.isGranted) Color.Green else Color.Red
-                        )
-                        Spacer(modifier = Modifier.height(8.dp))
-                    }
-                }
-            }
-            Spacer(Modifier.height(64.dp))
-            Button(onClick = { grant() }) {
-                Text("I agree")
             }
         }
     }
