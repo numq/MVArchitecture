c4af49d4771700c13829a743114757f1072ac286
numq <numq.dev@gmail.com>
2022-10-07 14:37:25 +0300

    Rename wrapper

---
+++ app/src/main/java/com/numq/mvarchitecture/wrapper/T.kt
@@ -0,0 +1,15 @@
+package com.numq.mvarchitecture.wrapper
+
+import arrow.core.Either
+import arrow.core.flatten
+import arrow.core.left
+import arrow.core.right
+
+fun <T> T.wrap(): Either<Exception, T> = runCatching { this }.fold({ it.right() },
+    { Exception(it.localizedMessage).left() })
+
+fun <T> T.wrapIf(condition: Boolean, exception: Exception): Either<Exception, T> =
+    Either.conditionally(
+        condition,
+        ifFalse = { exception },
+        ifTrue = { wrap() }).flatten()
